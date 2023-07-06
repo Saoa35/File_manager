bab5f29d0dc6dfcce3be54d7c9f8c80ade9ec734
Saoa35 <saoamah@gmail.com>
2023-07-06 10:20:40 +0400

    + isFolder to main.js

--- backend/routes/main.js
+++ backend/routes/main.js
@@ -1,5 +1,17 @@
+const fs = require("fs");
+
 module.exports = function (app) {
+  function isFolder(part) {
+    return fs.lstatSync(path).isSirectory() && fs.existsSync(path);
+  }
+
   app.get("/", (req, res) => {
-    res.end("main");
+    const base = "./files/";
+    let path = "";
+
+    if (isFolder(base + path)) {
+      let files = fs.readdirSync(base + path);
+      res.json(files);
+    }
   });
 };
