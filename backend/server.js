24f9b4dfc61311f85bf5396b02ce4618a518b0b5
Saoa35 <saoamah@gmail.com>
2023-07-06 12:15:18 +0400

    created logic for App component

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,17 @@ const app = express();
 
 const PORT = 8000;
 
+app.use(function (req, res, next) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With, content-type"
+  );
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  next();
+});
+
 require("./routes")(app);
 
 app.listen(PORT, (err) => {
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,31 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  const [data, setData] = useState();
+
+  useEffect(() => {
+    fetch("http://localhost:8000/").then((res) =>
+      res.json().then(
+        (result) => {
+          setData(result);
+        },
+        (error) => {
+          console.log(error);
+        }
+      )
+    );
+  }, []);
+
+  const checkDir = (file) => {
+    return file.dir ? <li>{file.name}</li> : <li>{file.name}</li>;
+  };
+
+  return (
+    <div className="App">
+      <ul>{data.files.map((item) => checkDir(item))}</ul>
+    </div>
+  );
 }
 
 export default App;
